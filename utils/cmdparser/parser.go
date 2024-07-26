acc0e554be4fd22a6d318571ac413c14b95157c7
A1in <xusenlin@bilibili.com>
2024-07-26 17:54:58 +0800

    暂时修复cmdparser的解析bug

--- utils/cmdparser/parser.go
+++ utils/cmdparser/parser.go
@@ -11,48 +11,96 @@ type Command interface {
 	CheckCommand() error
 }
 
+type t struct {
+	from  string
+	value string
+}
+
 // 命令解析，命令形式类似Linux命令，对于解析到bool字段的可选项可以省略value，但不要将该省略项置于可选项最后
 // e.g. "draw -bool_test -r 16:9 -out png girl" ✅ "draw -r 16:9 -out png -bool_test girl"❌
 // TODO 解析bug修复，具体见test
 func Parse(cmd string, target Command) (err error) {
+	cmdTags := make(map[string]*t)
+	rt := reflect.TypeOf(target).Elem()
+	rv := reflect.ValueOf(target).Elem()
+	requiredTagMaxIdx := -1
+	for i := 0; i < rt.NumField(); i++ {
+		field := rt.Field(i)
+		tag, ok := field.Tag.Lookup("cmd")
+		if !ok {
+			return fmt.Errorf("cmdparser can't find cmd tag in %v.%v", rt.Name(), field.Name)
+		}
+		cmdTags[tag] = nil
+		if strings.HasPrefix(tag, "required") {
+			requiredTagMaxIdx++
+		}
+	}
+	// 命令按空格分段
 	fields := strings.Fields(cmd)
-	fm := make(map[string]string)
-	rpi := 0
+	ri := 0
+	// 从起手式下一个开始
 	for i := 1; i < len(fields); {
 		if strings.HasPrefix(fields[i], "-") {
 			key := strings.TrimPrefix(fields[i], "-")
-			if i+1 < len(fields) && !strings.HasPrefix(fields[i+1], "-") {
-				fm[key] = fields[i+1]
-				i += 2
-			} else {
-				fm[key] = ""
-				i += 1
+			// 有对应tag
+			if _, ok := cmdTags[key]; ok {
+				if i+1 < len(fields) && !strings.HasPrefix(fields[i+1], "-") {
+					cmdTags[key] = &t{
+						from:  "user",
+						value: fields[i+1],
+					}
+					i += 2
+				} else {
+					// 在正常输入中只有bool类型的可选项能省略值
+					cmdTags[key] = &t{
+						from:  "cmdparser",
+						value: "true",
+					}
+					i += 1
+				}
 			}
 		} else {
-			key := fmt.Sprintf("required%v", rpi)
-			fm[key] = fields[i]
-			rpi++
-			i++
+			key := fmt.Sprintf("required%v", ri)
+			if cmdTags[key] == nil {
+				cmdTags[key] = &t{
+					from:  "user",
+					value: fields[i],
+				}
+			} else {
+				oldT := cmdTags[key]
+				cmdTags[key] = &t{
+					from:  "user",
+					value: oldT.value + " " + fields[i],
+				}
+			}
+			i += 1
+			// 判断后面还有没有required tag，有的话下次就填下一个
+			if ri < requiredTagMaxIdx {
+				ri++
+			}
 		}
 	}
-	rt := reflect.TypeOf(target).Elem()
-	rv := reflect.ValueOf(target).Elem()
+	// 回填
 	for i := 0; i < rt.NumField(); i++ {
 		field := rt.Field(i)
 		tag := field.Tag.Get("cmd")
-		if v, ok := fm[tag]; ok {
-			switch field.Type.Kind() {
-			case reflect.String:
-				rv.Field(i).SetString(v)
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				iv, err := strconv.Atoi(v)
+		switch field.Type.Kind() {
+		case reflect.String:
+			if cmdTags[tag] != nil && cmdTags[tag].from == "user" {
+				rv.Field(i).SetString(cmdTags[tag].value)
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if cmdTags[tag] != nil && cmdTags[tag].from == "user" {
+				iv, err := strconv.Atoi(cmdTags[tag].value)
 				if err != nil {
 					err = fmt.Errorf("cmdparser parse %v.%v err: %v", rt.Name(), field.Name, err.Error())
 					return err
 				}
 				rv.Field(i).SetInt(int64(iv))
-			case reflect.Bool:
-				rv.Field(i).SetBool(v == "true" || v == "")
+			}
+		case reflect.Bool:
+			if cmdTags[tag] != nil {
+				rv.Field(i).SetBool(cmdTags[tag].value != "")
 			}
 		}
 	}
--- utils/cmdparser/parser_test.go
+++ utils/cmdparser/parser_test.go
@@ -1,6 +1,7 @@
 package cmdparser_test
 
 import (
+	"a1in-bot-v3/modules/chat"
 	"a1in-bot-v3/modules/filewatcher"
 	"a1in-bot-v3/modules/magicpen"
 	"a1in-bot-v3/utils/cmdparser"
@@ -10,16 +11,21 @@ import (
 
 func Test_CommandParser(t *testing.T) {
 	cmd0 := &magicpen.DrawCommand{}
-	err := cmdparser.Parse("#draw white hair vtuber", cmd0)
+	err := cmdparser.Parse("#draw -help white hair vtuber", cmd0)
 	if err != nil || cmd0.CheckCommand() != nil {
 		t.Error(err)
 	}
-	// 已知bug: 这里只能解析到white
 	fmt.Printf("cmd0: %+v", cmd0)
 	cmd1 := &filewatcher.FileCommand{}
 	err = cmdparser.Parse("#file ls /home", cmd1)
 	if err != nil || cmd1.CheckCommand() != nil {
 		t.Error(err)
 	}
-	fmt.Printf("cmd0: %+v", cmd1)
+	fmt.Printf("cmd1: %+v", cmd1)
+	cmd2 := &chat.ChatCommand{}
+	err = cmdparser.Parse("#chat who are you", cmd2)
+	if err != nil || cmd2.CheckCommand() != nil {
+		t.Error(err)
+	}
+	fmt.Printf("cmd2: %+v", cmd2)
 }
